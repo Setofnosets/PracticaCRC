e769ccd198eec021468b291c8bb537dabd35a5b6
Setofnosets <locuartu@gmail.com>
2022-12-04 23:56:04 -0600

    Added mod2_div

--- crc.py
+++ crc.py
@@ -1,4 +1,5 @@
 from bitarray import bitarray
+from bitarray import util
 
 def compute(filename: str, divisor: bitarray, len_crc: int) -> tuple[bitarray, bitarray]:
     """    This function computes the CRC of a plain-text file
@@ -14,7 +15,20 @@ def mod2_div(dividend: bitarray, divisor: bitarray) -> bitarray:
              dividend: a bitarray holding the dividend
              divisor: a bitarray holding the divisor
              returns:    remainder: a bitarray holding the divider    """
-
+    while len(dividend) >= len(divisor):
+        j = 0
+        # Iterate until the first 1 is found
+        while dividend[j] == 0 or j == len(dividend)-1:
+            j += 1
+        # XOR operation
+        for i in range(len(divisor)):
+            dividend[j] = dividend[j] ^ divisor[i]
+            j += 1
+        # Remove leading zeros
+        dividend = util.strip(dividend, 'left')
+    if dividend == bitarray(''):
+        dividend = bitarray('0')
+    return dividend
 
 def burst_error(msg: bitarray, n: int, seed: int) -> bitarray:
     """    This function generates a error burst of length n
